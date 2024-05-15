0b96d8a77a06e7549535857365b1439dab5fb7dc
hideaki10 <senshikou1985@gmail.com>
2024-05-15 12:01:22 +0900

    add aws backup for s3 using tags example

--- lib/cdk-sample-01-stack.ts
+++ lib/cdk-sample-01-stack.ts
@@ -1,16 +1,55 @@
 import * as cdk from 'aws-cdk-lib';
+import * as events from 'aws-cdk-lib/aws-events';
+import { BackupPlan, BackupPlanRule, BackupVault, BackupResource } from 'aws-cdk-lib/aws-backup';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
+import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkSample01Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create aws s3
+    const bucket = new Bucket(this, "thisBucket", {
+      bucketName: "aws-backup-s3-sample-01",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSample01Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // create a tag for s3 bucket
+    cdk.Tags.of(bucket).add('bucket-backup', 'true')
+
+    // ceate backup vault
+    const backupVault = new BackupVault(this, "thisBackupVault", {
+      backupVaultName: "aws-backup-s3-sample-01-backup-vault",
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
+
+    // create backup plan
+    const backupPlan = new BackupPlan(this, 'thisBackupPlan', {
+      backupPlanName: "aws-backup-s3-sample-01-backup-plan",
+      backupVault: backupVault,
+    })
+
+    // add backup plan rule
+    backupPlan.addRule(
+      new BackupPlanRule({
+        ruleName: 'DailyBackup',
+        scheduleExpression: events.Schedule.cron({ minute: '0', hour: '4' })
+      })
+    )
+
+    // create backup selection
+    backupPlan.addSelection('thisBackupSelection', {
+      resources: [
+        BackupResource.fromTag('bucket-backup', 'true'),
+      ]
+    })
+
+    // create grant for backup vault
+    bucket.grantRead(new ServicePrincipal('backup.amazonaws.com'))
+
   }
 }
